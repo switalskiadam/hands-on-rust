f2c69c6a339cbf97ff5d72b74ac2aa4fa63df9f5
ASwitalski <aswitalski@blountfinefoods.com>
2024-05-13 15:35:44 -0500

    update to treehouse, completed program

--- learnrust/treehouse/src/main.rs
+++ learnrust/treehouse/src/main.rs
@@ -3,19 +3,40 @@ use std::io::stdin;
 #[derive(Debug)]
 struct Visitor {
     name: String,
-    greeting: String,
+    action: VisitorAction,
+    age: i8
+}
+
+#[derive(Debug)]
+enum VisitorAction {
+    Accept,
+    AcceptWithNote { note: String},
+    Refuse,
+    Probation
 }
 
 impl Visitor {
-    fn new(name: &str, greeting: &str) -> Self {
+    fn new(name: &str, action: VisitorAction, age: i8) -> Self {
         Self {
             name: name.to_lowercase(),
-            greeting: greeting.to_string(),
+            action,
+            age
         }
     }
 
     fn greet_visitor(&self) {
-        println!("{}", self.greeting);
+        match &self.action {
+            VisitorAction::Accept => println!("Welcome to the treehouse, {}", self.name),
+            VisitorAction::AcceptWithNote { note } => {
+                println!("Welcome to the treehouse, {}", self.name);
+                println!("{}", note);
+                if self.age < 21 {
+                    println!("Do not serve alcohol to {}", self.name)
+                }
+            }
+            VisitorAction::Probation => println!("{} is now a probationary memeber.", self.name),
+            VisitorAction::Refuse => println!("Do not allow {} in!", self.name),
+        }
     }
 }
 
@@ -29,11 +50,14 @@ fn what_is_your_name() -> String {
         .to_lowercase()
 }
 
+
 fn main() {
     let mut visitor_list = vec![
-        Visitor::new("bert", "Hello Bert, enjoy your treehouse."),
-        Visitor::new("steve", "Hi Steve. Your milk is in the fridge."),
-        Visitor::new("fred", "Wow, who invited Fred?"),
+        Visitor::new("bert", VisitorAction::Accept, 45),
+        Visitor::new("steve", VisitorAction::AcceptWithNote{
+             note: String::from("Hi Steve. Your milk is in the fridge.") 
+            }, 15),
+        Visitor::new("fred", VisitorAction::Refuse, 30),
     ];
 
     ///////// another option for creating the visitor list ///////////
@@ -58,11 +82,12 @@ fn main() {
                     break;
                 } else {
                     println!("{} is not on the visitor list.", name);
-                    visitor_list.push(Visitor::new(&name, "New friend"));
+                    visitor_list.push(Visitor::new(&name, VisitorAction::Probation, 0));
                 }
             }
         }
     }
     println!("The final list of visitors:");
     println!("{:#?}", visitor_list);
-}   
+
+}
